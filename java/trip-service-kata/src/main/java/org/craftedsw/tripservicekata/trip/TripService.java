1643551cb11630e0a6d42cb77ab0c902686ac847
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-14 14:15:48 +0100

    refactor

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -5,23 +5,17 @@ import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
-
         User loggedUser = getLoggedUser();
 
         if (loggedUser != null) {
+            return isFriendOf(user, loggedUser) ? getTripsBy(user) : Collections.emptyList();
 
-            boolean isFriend = isFriendOf(user, loggedUser);
-
-            if (isFriend) {
-                tripList = getTripsBy(user);
-            }
-            return tripList;
         } else {
             throw new UserNotLoggedInException();
         }
