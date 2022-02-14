2d72ffa71cc8a824b7f5a3ae4ab7096e9b4908cd
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-14 13:49:46 +0100

    test coverage

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/UserBuilder.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/UserBuilder.java
@@ -4,6 +4,8 @@ import org.craftedsw.tripservicekata.user.User;
 
 import java.util.stream.Stream;
 
+import static java.util.stream.Stream.*;
+
 
 class UserBuilder {
     private final Trip[] trips;
@@ -21,24 +23,19 @@ class UserBuilder {
     }
 
     public UserBuilder withFriends(User... user) {
-        Stream<User> newFriends = Stream.concat(Stream.of(this.friends), Stream.of(user));
+        Stream<User> newFriends = concat(of(this.friends), of(user));
         return new UserBuilder(this.trips, newFriends.toArray(User[]::new));
     }
 
     public UserBuilder withTrips(Trip... trips) {
-        Stream<Trip> newTrips = Stream.concat(Stream.of(this.trips), Stream.of(trips));
+        Stream<Trip> newTrips = concat(of(this.trips), of(trips));
         return new UserBuilder(newTrips.toArray(Trip[]::new), this.friends);
     }
 
     public User build() {
         User user = new User();
-
-        for (Trip trip : trips) {
-            user.addTrip(trip);
-        }
-        for (User friend : friends) {
-            user.addFriend(friend);
-        }
+        of(trips).forEach(user::addTrip);
+        of(friends).forEach(user::addFriend);
 
         return user;
     }
