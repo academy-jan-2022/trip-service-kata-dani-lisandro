c7875d52931b2a9e0bd4e55a3b30d96788b6063e
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-14 14:39:29 +0100

    refactor

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -4,7 +4,6 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -13,20 +12,13 @@ public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         return getLoggedUser()
-                .map(
-                    loggedUser -> isFriendOf(user, loggedUser) ? getTripsBy(user) : Collections.<Trip> emptyList()
-                )
+                .map(loggedUser -> isFriendOf(user, loggedUser) ? getTripsBy(user) : Collections.<Trip>emptyList())
                 .orElseThrow(UserNotLoggedInException::new);
     }
 
     private boolean isFriendOf(User user, User loggedUser) {
-        for (User friend : user.getFriends()) {
-            if (friend.equals(loggedUser)) {
-                return true;
-
-            }
-        }
-        return false;
+        return user.getFriends().stream()
+                .anyMatch(userFriend -> userFriend.equals(loggedUser));
     }
 
     protected List<Trip> getTripsBy(User user) {
@@ -34,7 +26,7 @@ public class TripService {
     }
 
     protected Optional<User> getLoggedUser() {
-		return Optional.ofNullable(UserSession.getInstance().getLoggedUser());
+        return Optional.ofNullable(UserSession.getInstance().getLoggedUser());
     }
 
 }
