6ac4812c5367ff20eff6426ff4c772a4bd44f67c
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-14 12:17:12 +0100

    empty trips when no friends green

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,25 +1,45 @@
 package org.craftedsw.tripservicekata.trip;
 
+
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TripServiceShould {
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class TripServiceShould {
+    public User loggedUser = null;
     @Test()
     void return_error_if_user_is_not_logged() {
         TripService tripService = new TestableTripService();
-        User user = new User();
 
-        Assertions.assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+        Assertions.assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(new User()));
     }
 
-    private static class TestableTripService extends TripService {
+    @Test() void
+    return_empty_trips_list_when_not_friends(){
+        TripService tripService = new TestableTripService();
+        User notFriend = new User();
+        notFriend.addFriend(new User());
+        notFriend.addTrip(new Trip());
+
+        loggedUser = new User();
+
+        List<Trip> trips = tripService.getTripsByUser(notFriend);
+
+        assertThat(trips.size(), is(0));
+    }
+
+     private class TestableTripService extends TripService {
 
         @Override
         protected User getLoggedUser() {
-            return null;
+            return loggedUser;
         }
     }
 }
